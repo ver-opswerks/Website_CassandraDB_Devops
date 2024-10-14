247ed1ab2120acf38dabe0c7834fdcdc03b94e8d
romeo <romeofrancobarro@gmail.com>
2024-10-14 13:38:13 +0800

    fixed data

--- src/opswerks-hub/src/components/Config.js
+++ src/opswerks-hub/src/components/Config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: 'http://172.104.38.128:80', // External ip of the frontend with protocol and port
+    apiBaseUrl: 'http://139.144.127.77:80', // External ip of the frontend with protocol and port
   };
   
 export default config;
